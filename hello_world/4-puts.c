a44215b9f9d541f5d6cc3f977d4655d2575aea18
Sami <5741@holbertonstudents.com>
2023-02-17 06:08:11 -0800

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ * wildcard backslash "\"" is used as an escape character,
+ * to protect a subsequent special character
+ * 
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
