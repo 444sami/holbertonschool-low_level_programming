af8695e8d34f2a360b1c26e8f19136a204768c51
Sami <5741@holbertonstudents.com>
2023-02-17 06:09:23 -0800

    betty fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,9 +5,9 @@
  * Return: always 0
  * wildcard backslash "\"" is used as an escape character,
  * to protect a subsequent special character
- * 
+ *
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
