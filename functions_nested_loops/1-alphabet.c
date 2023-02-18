e5139a526aa51c922c18b07a031d635bd300cb62
Sami <5741@holbertonstudents.com>
2023-02-18 07:07:32 -0800

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a new line
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char chars = 97;
+
+	for (; chars <= 122; chars++)
+	{
+		_putchar(chars);
+	}
+	_putchar('\n');
+}
