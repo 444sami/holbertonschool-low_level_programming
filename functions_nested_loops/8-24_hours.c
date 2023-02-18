b9eb620b648d6bd9e3389a0aa1c3cdd801ed710b
Sami <5741@holbertonstudents.com>
2023-02-18 08:15:20 -0800

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	int hou, min;
+
+	for (hou = 0; hou < 24; hou++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hou / 10) + '0');	/**first digit of hour*/
+			_putchar((hou % 10) + '0');	/**second digir of hour*/
+			_putchar(':');			
+			_putchar((min / 10) + '0');	/**first digit of minute*/
+			_putchar((min % 10) + '0');	/**secod digit of minute*/
+			_putchar('\n');
+		}
+	}
+}
