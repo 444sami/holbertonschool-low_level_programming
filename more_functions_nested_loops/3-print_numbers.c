7f5ce4415c91282e67cb42705248a3f29b8d4b4a
Sami <5741@holbertonstudents.com>
2023-02-23 05:18:56 -0800

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int num = '0';
+
+	for (; num <= 9; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
