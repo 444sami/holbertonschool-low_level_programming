7bc1baf9c593a218dd3c2783a30402ad4313c528
Sami <5741@holbertonstudents.com>
2023-02-24 09:45:10 -0800

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of square
+ * Return: square
+ */
+void print_square(int size)
+{
+	int x = 0;
+	int y = 0;
+
+	if (size < 0)
+	{
+		_putchar(10)
+	}
+	else
+	{
+		for (; x < y; x++)
+		{
+			for (; y < x; y++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+}
