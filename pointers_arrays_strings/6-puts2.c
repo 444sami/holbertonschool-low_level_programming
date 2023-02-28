9dcffcc93b30d4a458fffdd31a0cdcc773fe8ef0
Sami <5741@holbertonstudents.com>
2023-02-28 09:03:41 -0800

    task 6 fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,13 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; str[i]++)
 	{
-		if ((str[i] % 2) == 0)
+		if ((i % 2) == 0)
 		{
 			_putchar(str[i]);
 		}
 		else
 		{
 			continue;	/* si no uso 'continue' el loop se romperá */
-		}			/* llegará a printear 2 y al ser múltiplo */
-	}				/* no seguirá printeando 4, 6, 8..*/
+		}
 	_putchar(10);
 }
