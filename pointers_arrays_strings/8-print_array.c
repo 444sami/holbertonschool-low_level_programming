06acccd490044a67eaf4f4f0ca0877a5ee901518
Sami <5741@holbertonstudents.com>
2023-02-28 09:39:11 -0800

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - prints n elementts of an array of ints
+ * @a: array
+ * @n: numbers of elements
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+		i++;
+	}
+	putchar('\n');
+}
