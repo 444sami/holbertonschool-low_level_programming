f935d7560e1c3c86d60766459fde2c9b34707e93
Sami <5741@holbertonstudents.com>
2023-02-28 08:53:39 -0800

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other char of a string
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (str[i] = 0; str[i] != '\0'; str[i]++)
+	{
+		if ((i % 2) != 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue	/* si no uso 'continue' el loop se romperá */
+		}			/* llegará a printear 2 y al ser múltiplo */
+	}				/* no seguirá printeando 4, 6, 8..*/
+	_putchar(10);
+}
