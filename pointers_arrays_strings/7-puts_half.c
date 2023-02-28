8388b3801b6f97c9f5aa72b827091fb9c6d0754a
Sami <5741@holbertonstudents.com>
2023-02-28 09:29:54 -0800

    task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * puts_half - prints half a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i, n;
+
+	len = _strlen(str);
+
+	n = ((len - 1) / 2);
+
+	if ((len % 2) != 0)
+	{
+		n = ((len + 1) / 2)
+	}
+	for (i = n; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+#include "main.h"
+/**
+ * _strlen - return length of string
+ * @s: pointer
+ * Return: length (int)
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+		;
+	return (count);
+}
