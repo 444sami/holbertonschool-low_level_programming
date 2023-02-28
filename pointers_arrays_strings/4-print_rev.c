648580dc0b1cc3b4723ae1eb2cc575dd14dba895
Sami <5741@holbertonstudents.com>
2023-02-28 05:48:30 -0800

    task 4 fix

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -14,3 +14,17 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+#include "main.h"
+/**
+ * _strlen - return length of string
+ * @s: pointer
+ * Return: length (int)
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; count++)
+		;
+	return (count);
+}
