aba6c1bfdb9b081fe7894b748e65f23f1c8e36ab
Sami <5741@holbertonstudents.com>
2023-03-04 13:49:45 -0800

    task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *leet - encodes a string into 1337
+ * @str: string
+ * Return: encoded string
+ */
+char *leet(char *str)
+{
+	int i = 0;
+	int n = 0;
+	char s2[5] = {'a', 'e', 'o', 't', 'l'};
+	char s3[5] = {'4', '3', '0', '7', '1'};
+
+	for (; str[i] != '\0'; i++)
+	{
+		for (; n < 5; n++)
+		{
+			if (str[i] == s2[n] || str[i] == s2[n] - 32)
+			{
+				str[i] = s3[n];
+			}
+		}
+	}
+	return (str);
+}
