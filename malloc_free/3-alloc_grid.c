acd76189920fde9bdacfe8d94eb46afcf130de3a
Sami <5741@holbertonstudents.com>
2023-03-19 09:45:01 -0700

    task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -7,5 +7,42 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int n, i;
+	int **p;
 
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+
+	p = malloc(sizeof(*) * height);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (n = 0 ; n < height ; n++)
+	{
+		p[n] = malloc(sizeof(int) * width);
+
+		if (p[n] == NULL)
+		{
+			while (n >= 0)
+			{
+				free(p[n]);
+				n--;
+			}
+			free(p);
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0 ; p[n][i] ; i++)
+			{
+				p[n][i] = 0;
+			}
+		}
+	}
+	return (p);
 }
