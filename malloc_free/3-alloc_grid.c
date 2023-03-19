27f0919f645b0d4d9542f043be9d3b9ae6144447
Sami <5741@holbertonstudents.com>
2023-03-19 09:42:43 -0700

    task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * **alloc_grid - returns a pointer to a 2D array
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: pointer
+ */
+int **alloc_grid(int width, int height)
+{
+
+}
