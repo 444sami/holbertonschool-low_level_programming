ad0d5234c71df523f8be31f2ebd2738769995003
Sami <5741@holbertonstudents.com>
2023-03-21 16:38:05 -0700

    task 5

---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef dog_h
+#define dog_h
+#include <stdlib.h>
+/**
+ * struct dog - dogs structure
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
