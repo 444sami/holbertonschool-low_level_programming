2464ca3c47907efc9cbf7a546517aa9113fa1568
Sami <5741@holbertonstudents.com>
2023-03-23 17:40:44 -0700

    task 8 unfinished

---
+++ pointers_arrays_strings/even_more_pointers/8-print_diagsums.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_diagsums - print sums of two diagonals
+ * of a square matrix of integers
+ * @a: pointer
+ * @size: size
+ * Return: sum of diagonals
+ */
+void print_diagsums(int *a, int size)
+{
+}
