da926e7b69d142edad4805775d4368b6aa898007
Sami <5741@holbertonstudents.com>
2023-03-24 17:01:52 -0700

    organizing files

--- pointers_arrays_strings/even_more_pointers/7-print_chessboard.c
+++ pointers_arrays_strings/even_more_pointers/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_chessboard - prints the chessboard
  * @a: pointer
@@ -6,5 +7,18 @@
  */
 void print_chessboard(char (*a)[8])
 {
+	int i;
+	int j;
 
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", (a[i][j]));
+			if (j == 7)
+			{
+				printf("\n");
+			}
+		}
+	}
 }
