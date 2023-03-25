626843045f23b9e10cf57a70ecedbe418e16d270
Sami <5741@holbertonstudents.com>
2023-03-24 17:00:04 -0700

    task 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer
+ * Return: chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", (a[i][j]));
+			if (j == 7)
+			{
+				printf("\n");
+			}
+		}
+	}
+}
