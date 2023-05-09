e225bfb930066a7f03806d5f6777f338acf11586
Sami <5741@holbertonstudents.com>
2023-05-09 15:49:56 -0700

    task 4 finished

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,12 +2,20 @@
 /**
  * _pow_recursion - returns the value of x
  * raised to the power of y
- * @x: x axis
- * @y: y axis
+ * @x: x
+ * @y: y
  * Return: if y < 0 return -1, if success
  * return x raised to power of y
  */
 int _pow_recursion(int x, int y)
 {
-
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (_pow_recursion(x, y - 1) * x);
 }
